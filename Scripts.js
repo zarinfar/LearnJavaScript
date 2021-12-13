df0cae32972683e9cbe496ad07a1a3ff0b31c0c2
Reza <maminzarinfar1001@gmail.com>
2021-12-13 18:00:25 +0330

    first commit

---
+++ Scripts.js
@@ -0,0 +1,38 @@
+const person = {
+    firstName : "Amin",
+    lastName  : "Doe",
+    age     : 50,
+    eyeColor  : "blue"
+};
+document.getElementById("demo").innerHTML = person.firstName + " have " + person.age + " years old"
+
+const  person2 = {
+    fistName2 : "reza",
+    LastName : "ahmadi",
+    Age      : 28,
+    FatherName: "Mohamad"
+                };
+
+document.getElementById("person2").innerHTML = "My Name is " + person2.fistName2 + " " + person2.LastName +
+    " and My father name is " + person2.FatherName
+
+function Test () {
+    let a = document.getElementById("Value1").value;
+    let b = document.getElementById("Value2").value;
+
+    if (a == b) {
+        document.getElementById("Displayer").innerHTML = "Mosavi ast" ;
+    }
+
+    if (a < b) {
+        document.getElementById("Displayer").innerHTML = a + " az " + b + " kuchiktare";
+    }
+
+    if (a > b) {
+        document.getElementById("Displayer").innerHTML = a + " az " + b + " Bozorgtare";
+    }
+
+}
+
+
+
