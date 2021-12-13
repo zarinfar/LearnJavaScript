ef0be90f628bcb4cbc307aea534c4476a7952da1
Reza <maminzarinfar1001@gmail.com>
2021-12-13 20:11:50 +0330

    Add more Properties

--- Scripts.js
+++ Scripts.js
@@ -1,3 +1,4 @@
+//declare object 1
 const person = {
     firstName : "Amin",
     lastName  : "Doe",
@@ -5,7 +6,7 @@ const person = {
     eyeColor  : "blue"
 };
 document.getElementById("demo").innerHTML = person.firstName + " have " + person.age + " years old"
-
+//declare object 2
 const  person2 = {
     fistName2 : "reza",
     LastName : "ahmadi",
@@ -13,6 +14,10 @@ const  person2 = {
     FatherName: "Mohamad"
                 };
 
+document.getElementById("person2").innerHTML = "My Name is " + person2.fistName2 + " " + person2.LastName +
+    " and My father name is " + person2.FatherName
+
+//declare object 3
 const Food = {
     Name: "Pizza",
     Price: 150000
@@ -20,10 +25,40 @@ const Food = {
 
 document.getElementById("food").innerHTML = Food.Name + " is " + Food.Price + " Rial"
 
-document.getElementById("person2").innerHTML = "My Name is " + person2.fistName2 + " " + person2.LastName +
-    " and My father name is " + person2.FatherName
+//object 4
+
+const Home = {
+                Address : "Niavaran, Kuhestan",
+                Metrazh : 200,
+                Hayat : "Darad",
+                Anbari : "Darad",
+                Garmayesh: "Shufazh",
+                Panjareh : 3,
+                Balkon: "Darad"
+            }
 
 
+
+document.getElementById("Home").innerHTML = "In melk dar mantagheye " + Home.Address + " Mibashad." +
+    "Metrazhe in melk " + Home.Metrazh + " metr Mibashad." + " in melk hayat " + Home.Hayat + " in melk Anbari " + Home.Anbari +
+    " Garmayeshe in khane " + Home.Garmayesh + " Mibashad. " + "in melk daraye " + Home.Panjareh + " Panjareh mibashad" +
+    "in melk balkon 20 metri " + Home.Balkon
+
+const Car = {
+    Name     : "Z4",
+    Company  : "BMW",
+    Model    : "2021",
+    Color    : "Red",
+    Automate : "Yes",
+    Weight   : "800 KG"
+}
+
+document.getElementById("Car").innerHTML = "The Car Name is " + Car.Name + ". The Company is " + Car.Company +
+    ". The Model is " + Car.Model + ". The Color is " + Car.Color + ". is it Automate? " + Car.Automate + ". The Weight is "
++ Car.Weight + "."
+
+
+    // Function 1 > Moghyese adadaha
 
 function Test () {
     let a = Number(document.getElementById("Value1").value);
@@ -44,4 +79,4 @@ function Test () {
 }
 
 
-
+//
