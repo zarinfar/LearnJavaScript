e03e522387db619a7f55cd0bcb20b165c32da1ec
zarinfar <maminzarinfar1001@gmail.com>
2021-12-22 00:01:14 +0330

    Moratab ssazi

--- Scripts.js
+++ Scripts.js
@@ -6,6 +6,8 @@ const person = {
     eyeColor  : "blue"
 };
 document.getElementById("demo").innerHTML = person.firstName + " have " + person.age + " years old"
+
+
 //declare object 2
 const  person2 = {
     fistName2 : "reza",
@@ -44,6 +46,7 @@ document.getElementById("Home").innerHTML = "In melk dar mantagheye " + Home.Add
     " Garmayeshe in khane " + Home.Garmayesh + " Mibashad. " + "in melk daraye " + Home.Panjareh + " Panjareh mibashad" +
     "in melk balkon 20 metri " + Home.Balkon
 
+//object 5
 const Car = {
     Name     : "Z4",
     Company  : "BMW",
@@ -82,5 +85,49 @@ function Test () {
 
 }
 
-
+// Function 2 > Input Counter
+function Counterlength() {
+    let Counter = document.getElementById("input-counter").value;
+    let length = Counter.length;
+    document.getElementById("counter").innerHTML = length;
+}
 //
+
+
+// Play with Array
+
+    let Array = ["Amin", "Minoo", "Saeid","sara","Ahmad"]
+    Array.sort();
+    document.getElementById("Array").innerHTML = Array;
+//Display Time
+    Time = Date();
+    document.getElementById("Time").innerHTML = Time.slice(16, 24);
+//Display Year
+    Year = new Date();
+    document.getElementById("Year").innerHTML =  Year.getFullYear();
+//Display Month
+    Month = new Date();
+    Month2 = Month.getMonth();
+    Month3 = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    document.getElementById("Month").innerHTML = Month3[Month2];
+
+// Compare date
+
+    let text;
+    today = new Date();
+    someday = new Date();
+
+    Year1 = 2120;
+    Month4 = 1;
+    Day = 22;
+
+    someday.setFullYear(Year1, Month, Day);
+
+    if (someday > today) {
+      text = "Today is before " + Month3[Month4] + " " +Day + ", " + Year1;
+    } else {
+      text = "Today is after " + Month3[Month4] + " " +Day + ", " + Year1;
+    }
+        document.getElementById("text2text2").innerHTML = text;
+
+
